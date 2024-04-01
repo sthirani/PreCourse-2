af72e53f6b77f72047f0923f0df68660d32c9194
sthirani <32892047+sthirani@users.noreply.github.com>
2024-04-01 13:17:54 -0700

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,3 +1,7 @@
+/* Time Complexity : O(nlogN) Worst case O(n2)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
 class QuickSort 
 { 
     /* This function takes last element as pivot, 
@@ -8,11 +12,33 @@ class QuickSort
        of pivot */
     void swap(int arr[],int i,int j){
         //Your code here   
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     
     int partition(int arr[], int low, int high) 
     { 
+
    	//Write code here for Partition and Swap 
+        int pivot = arr[high];
+        int pivotIndex = high;
+        while(low<high){
+            while(arr[low]<=pivot && low<high){
+                low++;
+            }
+            while(arr[high]>=pivot && low<high){
+                high--;
+            }
+            if(low<high){
+                swap(arr,low,high);
+            }
+           
+        }
+        System.out.println("Swapping pivot elements "+ arr[pivotIndex] +"and " + arr[high]);
+        swap(arr,pivotIndex,high);
+        return high;
+
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
@@ -20,8 +46,14 @@ class QuickSort
       high  --> Ending index */
     void sort(int arr[], int low, int high) 
     {  
-            // Recursively sort elements before 
-            // partition and after partition 
+        // Recursively sort elements before 
+        // partition and after partition 
+        if(low<high){
+
+            int pivot = partition(arr,low,high);
+            sort(arr,low,pivot-1);
+            sort(arr,pivot+1,high);
+        }
     } 
   
     /* A utility function to print array of size n */
@@ -46,3 +78,17 @@ class QuickSort
         printArray(arr); 
     } 
 } 
+/*Ouput:
+ * 
+ * Pivot index 5
+Swapping elements 10and 1
+Swapping pivot elements 5and 7
+Pivot index 5
+Swapping pivot elements 7and 8
+Pivot index 5
+Swapping pivot elements 8and 9
+Pivot index 5
+Swapping pivot elements 9and 10
+sorted array
+1 5 7 8 9 10 
+ */
