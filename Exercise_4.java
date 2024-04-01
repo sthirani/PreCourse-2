cbf945f822549139f404e815e09907ad1fb61c99
sthirani <32892047+sthirani@users.noreply.github.com>
2024-04-01 13:18:33 -0700

    Update Exercise_4.java

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,20 +1,67 @@
+
+// Time Complexity : O(nlogn)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
 class MergeSort 
 { 
     // Merges two subarrays of arr[]. 
     // First subarray is arr[l..m] 
     // Second subarray is arr[m+1..r] 
-    void merge(int arr[], int l, int m, int r) 
+    void merge(int arr[],int l, int mid , int r) 
     {  
-       //Your code here  
+   
+      int len1 = mid-l+1;
+      int len2 = r-mid;
+      int[] arr1 = new int[len1];
+      
+      int[] arr2 = new int[len2];
+      int m = 0;
+      int n = 0;
+      int k = l;
+    for(int i =0; i<len1;i++){
+        arr1[i]=arr[i+l];
+    }
+    for(int i =0; i<len2;i++){
+        arr2[i]=arr[mid+i+1];
+    }
+
+      while(m<len1 && n<len2){
+        if(arr1[m]<= arr2[n]){
+            arr[k++] = arr1[m++];
+        } else {
+            arr[k++]=arr2[n++];
+        }
+      }
+       while(m<len1){
+        arr[k++] = arr1[m++];
+       }
+       while(n<len2){
+        arr[k++]=arr2[n++];       
+       }
+      
     } 
   
     // Main function that sorts arr[l..r] using 
     // merge() 
     void sort(int arr[], int l, int r) 
     { 
-	//Write your code here
-        //Call mergeSort from here 
+        if(l<r){
+            
+        int mid = l + (r-l)/2;
+
+        sort(arr, l, mid);
+
+        sort(arr, mid+1, r);
+
+        merge(arr, l, mid, r);
+
     } 
+}
   
     /* A utility function to print array of size n */
     static void printArray(int arr[]) 
@@ -34,9 +81,17 @@ class MergeSort
         printArray(arr); 
   
         MergeSort ob = new MergeSort(); 
-        ob.sort(arr, 0, arr.length-1); 
+         ob.sort(arr, 0, arr.length-1); 
   
         System.out.println("\nSorted array"); 
         printArray(arr); 
     } 
 } 
+/*
+ * Output
+ * Given Array
+12 11 13 5 6 7 
+
+Sorted array
+5 6 7 11 12 13 
+ */
